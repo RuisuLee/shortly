477ae77bc13d2c1bcd3ddc8c3ae8c4fa4d26fa3a
Zaitseva Aleksandra <ruisu.lee@gmail.com>
2023-06-15 11:17:39 +0400

    feat/layout: added shorten component

---
+++ src/components/Shorten/Shorten.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import {
+  SHORTEN_BUTTON_TEXT,
+  SHORTEN_ERROR,
+  SHORTEN_PLACEHOLDER,
+} from "../../constants";
+import "./Shorten.scss";
+
+export const Shorten = () => {
+  const [url, setUrl] = useState("");
+  const [touched, setTouched] = useState(false);
+  const isUrlInvalid = url === "";
+  return (
+    <div className="shorten__wrapper">
+      <div className="shorten">
+        <label htmlFor="shorten"></label>
+        <input
+          name="shorten"
+          type="text"
+          placeholder={SHORTEN_PLACEHOLDER}
+          className={`shorten__input ${
+            isUrlInvalid && touched ? "shorten__input--invalid" : ""
+          }`}
+          value={url}
+          onBlur={() => setTouched(true)}
+          onChange={(e) => {
+            setUrl(e.target.value);
+          }}
+        />
+        {isUrlInvalid && touched && (
+          <div className="shorten__error">{SHORTEN_ERROR}</div>
+        )}
+      </div>
+      <button
+        type="button"
+        className={`shorten__button ${
+          isUrlInvalid && touched ? "shorten__button--invalid" : ""
+        }`}
+      >
+        {SHORTEN_BUTTON_TEXT}
+      </button>
+    </div>
+  );
+};
--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,7 @@ export const NAV_ITEMS = ["Features", "Pricing", "Resources"];
 
 export const LOGIN = "Login";
 export const SIGN_UP = "Sign Up";
+
+export const SHORTEN_PLACEHOLDER = "Shorten a link here...";
+export const SHORTEN_BUTTON_TEXT = "Shorten It!";
+export const SHORTEN_ERROR = "Please add a link";
